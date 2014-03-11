68068a14a9af39b6097459ad60e6114f408c7551
niquola <niquola@gmail.com>
2014-03-11 11:42:05 +0400

    merge into one file

--- src/arroute/core.clj
+++ src/arroute/core.clj
@@ -1,3 +1,69 @@
 (ns arroute.core
   (:require
+    [clojure.zip :as z]
     [clojure.string :as str]))
+
+(defn to-url
+  "build url from pattern and params"
+  [pattern & [params]]
+  (->>
+    (reduce
+      (fn [acc item]
+        (let [val (if (keyword? item) (item params) item)]
+          (conj acc val)))
+      [] pattern)
+    (str/join "/")))
+
+(defn- pathify [path] ;move to url
+  (str/split path #"/"))
+
+(defn match-url
+  "match url by pattern and return hash of params"
+  [url pattern]
+  (let [url-parts (pathify url)
+        pairs (map vector pattern url-parts)]
+    (and
+      (= (count url-parts) (count pattern))
+      (reduce
+        (fn [acc [v1 v2]]
+          (if (or (= nil acc)
+                  (and (not (keyword? v1))
+                       (not (= v1 v2))))
+            nil
+            (if (keyword? v1) (assoc acc v1 v2) acc)))
+        {}
+        pairs))))
+
+(defn def-routes
+  "create routes zipper"
+  [& rts]
+  (z/zipper
+    (constantly true)
+    #(:nested %)
+    (fn [node children] (assoc node :nested children))
+    {:nested rts}))
+
+(defn- current-path [zp-cur]
+  (mapcat #(:path %) (conj (z/path zp-cur) (z/node zp-cur))))
+
+(defn get-attr [zp attr-name]
+"get attribute from node or his parent nodes"
+  (loop [loc zp]
+    (if (contains? (z/node loc) attr-name)
+      (attr-name (z/node loc))
+      (if (z/end? loc)
+        nil
+        (recur (z/up loc))))))
+
+(defn find-route-rule
+  "get routes-zipper and method path pair
+  return zipper cursor if matched else nil"
+  [routes-zipper [meth path]]
+  (loop [loc routes-zipper]
+    ; TODO: use pathified path for efficiency
+    (if (and (match-url path (current-path loc))
+             (= meth (get-attr loc :method)))
+      loc
+      (if (z/end? loc)
+        nil
+        (recur (z/next loc))))))
--- test/arroute/core_test.clj
+++ test/arroute/core_test.clj
@@ -1,7 +1,40 @@
 (ns arroute.core-test
   (:use midje.sweet)
-  (:use arroute.core)
-  (:require
-    [clojure.walk :as w]
+  (:use arroute.core))
+
+(fact "represent url as array"
+
+       (def users-profile ["users" :id "profile"])
+       (def posts ["blog" :user "posts" :filter])
+
+       (to-url users-profile {:id 1})  => "users/1/profile"
+       (match-url "unexisting/1/profile" users-profile) => nil
+       (match-url "users/1/profile" users-profile) => {:id "1"}
+       (match-url "users/nicola/profile" users-profile) => {:id "nicola"}
+       (match-url "blog/nicola/posts/last" posts) => {:filter "last" :user "nicola"}
+       )
+
+(def routes
+  (def-routes
+    {:path ["users"] :ns 'users
+     :nested [{:method :GET :desc "List users"}
+              {:method :POST :desc "Create user" :auth [:admin]}
+              {:path [:id]
+               :nested
+               [{:method :GET :desc "Show user"}
+                {:method :PUT :desc "Update user"}
+                {:method :POST :path ["activate"] :action 'activate}
+                ]}
+              ]}
     ))
+
+(fact "route is hash-map & can be nested"
+      (find-route-rule
+        routes
+        [:POST "user/5/activate"])  => nil
+
+      (get-attr
+        (find-route-rule
+          routes [:POST "users/5/activate"])
+        :action)  => 'activate)
 
